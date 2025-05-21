ca52191fbea9772eb318abb94deacd36e4d37c05
Ahmed Abbasi <ahmed-abbasi@outlook.com>
2025-05-21 23:24:09 +0200

    Update code to comply with refactored contract

--- client/src/lib/encrypt.ts
+++ client/src/lib/encrypt.ts
@@ -1,28 +1,29 @@
 import { randomBytes } from "@noble/ciphers/webcrypto";
-import { readFromContract, writeToContract } from "./viemHelpers";
+import {
+    connectWallet,
+    readFromContract,
+    writeToContract,
+} from "./viemHelpers";
 import { chacha20poly1305 } from "@noble/ciphers/chacha";
-import { split } from 'shamir-secret-sharing';
+import { split } from "shamir-secret-sharing";
 import { x25519 } from "@noble/curves/ed25519";
 import { toHex } from "viem";
 
 const dummyData = "Hello, World!";
 
-
 export async function uploadRecord(patientAddress: `0x${string}`) {
-    // Request public keys
-    const physicanPublicKey = (await readFromContract<any>({functionName: 'addressToAgent', args: ['0x07754fF521386988000Ea6600410764E167B6D12']}))[2].split("0x")[1];
-    const patientPublicKey = (await readFromContract<any>({functionName: 'addressToAgent', args: [patientAddress]}))[2].split("0x")[1];
-    const hospitalPublicKey = (await readFromContract<any>({functionName: 'addressToAgent', args: ['0x8919Ed732B18b6b9d6297817E2984EEc79087a27']}))[2].split("0x")[1];
-    const oracle1PublicKey = (await readFromContract<any>({functionName: 'getOracle', args: [0]})).publicKey.split("0x")[1];
-    const oracle2PublicKey = (await readFromContract<any>({functionName: 'getOracle', args: [1]})).publicKey.split("0x")[1];
+    const { account: physicianAccount } = await connectWallet();
 
-    const agents = [
-        {role: 0, publicKey: hospitalPublicKey},
-        {role: 2, publicKey: physicanPublicKey},
-        {role: 1, publicKey: patientPublicKey},
-        {role: 3, publicKey: oracle1PublicKey},
-        {role: 3, publicKey: oracle2PublicKey},
-    ];
+    // Request public keys
+    const agents = (
+        await readFromContract<any>({
+            functionName: "getAgents",
+            args: [patientAddress, physicianAccount],
+        })
+    ).map((agent: any) => ({
+        ...agent,
+        publicKey: agent.publicKey.split("0x")[1],
+    }));
 
     const fileKey = randomBytes(32);
     const fileNonce = randomBytes(12);
@@ -35,7 +36,7 @@ export async function uploadRecord(patientAddress: `0x${string}`) {
     const ciphertext = encryptedContent.slice(0, -16);
 
     // const shares = await split(fileKey, 5, 3);
-    const shares = (await split(fileKey, 5, 3)).map(share => share.slice(1));
+    const shares = (await split(fileKey, 5, 3)).map((share) => share.slice(1));
 
     const encryptedShares = await Promise.all(
         agents.map(async (agent, index) => {
@@ -51,7 +52,7 @@ export async function uploadRecord(patientAddress: `0x${string}`) {
             const encryptedShare = shareCipher.encrypt(shares[index]);
             const shareAuthTag = encryptedShare.slice(-16);
             const shareCiphertext = encryptedShare.slice(0, -16);
-            
+
             return [
                 agent.role,
                 toHex(shareCiphertext),
@@ -61,7 +62,7 @@ export async function uploadRecord(patientAddress: `0x${string}`) {
             ];
         })
     );
-    
+
     // IPFS storage logic here to store ciphertext;
 
     const record = [
@@ -71,7 +72,9 @@ export async function uploadRecord(patientAddress: `0x${string}`) {
         toHex(authTag),
     ];
 
-    const txHash = await writeToContract({functionName: 'addRecord', args: [patientAddress, record]})
+    const txHash = await writeToContract({
+        functionName: "addRecord",
+        args: [patientAddress, record],
+    });
     console.log(txHash);
-
 }
